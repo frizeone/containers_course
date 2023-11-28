3aef9aeebb98e2432477f0a8089c0a967a6e7614
Скрипников Ростислав Станиславович <skripnikov_rs@msk.miit.ru>
2023-11-28 14:57:17 +0300

    Make 3_3

--- 2023_containers/app/resource_containers_3_3/const.js
+++ 2023_containers/app/resource_containers_3_3/const.js
@@ -1,13 +1,13 @@
 /**
  * Created by vasa on 12.11.13. Modified by vitality on 29.06.21
  */
-var gnMaxPages = 46; // число слайдов в разделе
-angular.module('course').value('razdelName', 'Автоматическая установка маршрутов на станциях МЦК');
+var gnMaxPages = 35; // число слайдов в разделе
+angular.module('course').value('razdelName', 'Технические операции');
 angular.module('course').value('sectionName', '');
 angular.module('course').value('pageCount', gnMaxPages);
 var SdanoFlag = true;
 
-var InteractionFlag = []; // указание слайдов с интерактивностями
+var InteractionFlag = [9,11, 18, 23, 24, 25, 29, 33]; // указание слайдов с интерактивностями
 
 var dlina; // подобрано эмпирически
 switch (gnMaxPages) {
